fb9c15021f8e84ea2eea57bbef8837556f97a1de
Nenad Stanojev <NND.STANOJEV@gmail.com>
2018-02-14 12:42:33 +0100

    Add files via upload

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+########## MySql details (Replace with yours) #############
+$username = "root"; //mysql username
+$password = ""; //mysql password
+$hostname = "localhost"; //hostname
+$databasename = 'ajax'; //databasename
+
+//connect to database
+$mysqli = new mysqli($hostname, $username, $password, $databasename);
+
+
+?>
